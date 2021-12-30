b61940bea870d8132f97cac7833dce91b96f3579
Roman Art <root@romart.dev>
2021-12-30 18:05:13 +0300

    Pass files from CLI into lexer
    Also add tracking of lineno and position in scanning file

--- src/main.c
+++ src/main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include "hello.h"
+#include "lex.h"
+
+int num_lines = 0;
+int num_chars = 0;
 
 int main(int argc, char** argv) {
-  return foo(42);
+  if (argc < 2) return -1;
+  argc--; argv++;
+  for (int i = 0; i < argc; ++i) {
+    num_lines = num_chars = 0;
+    const char* inputFile = argv[i];
+    yyin = fopen(inputFile, "r");
+    yylex();
+    printf("File %s contains %d lines and %d chars\n", inputFile, num_lines, num_chars);
+  }
+  return ;
 }
