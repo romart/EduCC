f8ae9c0ec0dfa247a512c2efe84eafc204885963
Roman Art <root@romart.dev>
2022-01-12 18:27:56 +0300

    Properly support string literals and comments in lexer
    Refactored main function a bit

--- src/main.c
+++ src/main.c
@@ -5,17 +5,38 @@
 int num_lines = 0;
 int num_chars = 0;
 
+
+static void processFileTokens() {
+  
+}
+
+static void processInputFile(const char* inputFile) {
+
+    FILE* opened = fopen(inputFile, "r");
+    if (opened != NULL) {
+      yyin = opened;
+      printf("Scanning file %s...\n", inputFile);
+      int token = -1;
+      while (token = yylex()) {
+        printf("Token ID: %d (at line %d), its text '%s'\n", token, num_lines + 1, yytext);
+      }
+      printf("File ended (%d lines, %d chars).\n", num_lines, num_chars); 
+      // yylex();
+      // printf("File %s contains %d lines and %d chars\n", inputFile, num_lines, num_chars);
+      fclose(opened);
+    } else {
+      printf("Cannot open file %s\n", inputFile);
+    }
+}
+
+
 int main(int argc, char** argv) {
   if (argc < 2) return -1;
   argc--; argv++;
   for (int i = 0; i < argc; ++i) {
     num_lines = num_chars = 0;
     const char* inputFile = argv[i];
-    yyin = fopen(inputFile, "r");
-    if (yyin != NULL) {
-      yylex();
-      printf("File %s contains %d lines and %d chars\n", inputFile, num_lines, num_chars);
-    }
+    processInputFile(inputFile);
   }
   return 0;
 }
