864f0fdb9f0b620c6bbb4baba7b01d00871d4320
Roman Art <root@romart.dev>
2022-01-18 17:15:43 +0300

    Add AST render

---
+++ include/treeDump.h
@@ -0,0 +1,19 @@
+
+#ifndef __TREE_DUMP_H__
+#define __TREE_DUMP_H__ 1
+
+#include <stdio.h>
+
+#include "tree.h"
+
+// dump
+
+int dumpAstFile(FILE *output, AstFile *file);
+int dumpAstExpression(FILE *output, AstExpression *expr);
+int dumpAstStatement(FILE *output, AstStatement *stmt);
+int dumpDeclaration(FILE *output, Declaration *declaration);
+
+int dumpTypeRef(FILE *output, TypeRef *type);
+int dumpTypeDesc(FILE *output, TypeDesc *desc);
+
+#endif // __TREE_DUMP_H__
