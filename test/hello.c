2fab4ac884dfc01287773393655b47aa099c6b12
Roman Art <root@romart.dev>
2022-01-20 18:23:48 +0300

    Update test

--- test/hello.c
+++ test/hello.c
@@ -9,7 +9,9 @@ int foo(int x, t_i xx, t_i **y, pt_i pz, pt_i *ppz, ppt_i a, ppt_i *pa, ppt_i **
   return 0;
 }
 
-int qux(int (******f)(double (*****)(struct S))) {}
+int (*(qux(int (******f)(double (*****)(struct S*))))) {}
+
+int *fxx(int p);
 
 int *(***(x)[20][30][40])(int *(*)(int, int **(**)(int, int**)), int);
 
@@ -17,19 +19,28 @@ int *(*xx[10])(int);
 
 typedef int (*f_t)(int, int);
 
+enum E1 {
+  EC_1 = -1,
+  EC_2 = EC_1 + 15,
+  EC_3
+};
+
 f_t fax(int a, int b) {
-  int c;
+  int c = EC_3;
   for (c = a; c < b; ++c) {
     printf("%d %d %d", a, b, c);
   }
 }
 
+static x;
 int foo1(void) {
-
+  int xx = 42;
+  long double xxx = 10;
+  unsigned char xxxx = xx + xxx;
 }
 
 int foo2(void) {
-
+  struct SL { int a; } a = { 0 }, b = { 0 }, *c = NULL, d;
 }
 
 int f() {
@@ -46,10 +57,14 @@ struct S;
 struct SS {
     int a;
     int b, d;
-    struct {
+    typedef struct {
         double x;
         struct XXX ****xx;
-    } sfx;
+    } ss1, *ss2, **ss3;
+
+    ss1 v1, *pv1, **pv1;
+    ss2 v2, *pv2, **pv2;
+    ss3 v3, *pv3, **pv3;
 };
 
 struct {
