c30b98cee6b6e0bf7a219d9b5483c3254ff91ab6
Roman Art <root@romart.dev>
2022-01-20 18:22:08 +0300

    Implement compiler symbols (no Scope support yet)
     - rethink symbol representation
     - move their code into sema.h
     - implement related API

--- include/parser.h
+++ include/parser.h
@@ -12,38 +12,6 @@ AstConst* eval(ParserContext *ctx, AstExpression* expression);
 void parseError(ParserContext *ctx, const char* fmt, ...);
 void parseWarning(ParserContext *ctx, const char* fmt, ...);
 
-enum SymbolKind {
-    FunctionSymbol = 1,
-    UnionSymbol,
-    StructSymbol,
-    TypedefSymbol,
-    VariableSymbol,
-    EnumSymbol, /** TODO: not sure about it */
-    EnumConstSymbol
-};
-
-struct VariableDesc;
-
-typedef struct _Symbol {
-    int kind;
-    const char* name; /** struct/union/enum is referenced via "$$name" or "|$name" or "#$enum"*/
-    union {
-        struct {
-            FunctionTypeDescriptor *typeDescriptor;
-            AstDeclaration *definition;
-        } functionSymbol; // FunctionSymbol
-        TypeDesc *typeDescriptor; // StructSymbol | UnionSymbol | EnumSymbol, struct S;
-        TypeRef * typeref; // TypedefSymbol, typedef struct TS* ts_t;
-        struct VariableDesc* variableDesc; // VariableSymbol, int a = 10;
-        struct _EnumConstant *enumerator; // EnumConstSymbol
-    };
-} Symbol;
-
-typedef struct _Scope {
-    struct _Scope* parent;
-    HashMap* symbols;
-} Scope;
-
 #define TokenTextCacheSize 1024
 
 typedef struct _Token {
@@ -58,13 +26,15 @@ typedef struct _Token {
     struct _Token *next;
 } Token;
 
+struct _Scope;
+
 typedef struct _ParserContext {
     AstFile* parsedFile;
 
     yyscan_t scanner;
 
-    Scope* rootScope;
-    Scope* currentScope;
+    struct _Scope* rootScope;
+    struct _Scope* currentScope;
 
     Token *firstToken;
     Token *token;
