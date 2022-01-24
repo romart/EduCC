526ce83cf174f5572515844bd758b36a5ffd5f52
Roman Art <root@romart.dev>
2022-01-24 19:13:55 +0300

    Update test data
     - fix testData of fixed tests
     - add test for initializers

---
+++ test/testData/parser/negative/expressions/initializers.c
@@ -0,0 +1,17 @@
+
+struct S { float x; int *ptr; int *ptr2; };
+struct S s1 = { { { (void*)0, 01.f, 5 } , 0 }, { 0, 0.1f, "ccc" } , 0 };
+int a1[20];
+int a2[] = a1;
+int *pa = a1;
+int a3[];
+int a4[20] = 0;
+int b[][20] = {} ;
+struct S s2 = { 0 };
+int i = 10;
+float f = pa;
+float f2 = 1.0f;
+int i2 = f2;
+int i3 = s2;
+void x;
+int a5[]={ { { 0, 0}, 0, { 0, 0}}, { 0}, {0, { }}}
