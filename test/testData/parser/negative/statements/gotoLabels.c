c5eedaf846cf96a891f78aaf51e1697eadd77b99
Roman Art <root@romart.dev>
2022-01-28 02:28:40 +0300

    Add goto/label tests

---
+++ test/testData/parser/negative/statements/gotoLabels.c
@@ -0,0 +1,28 @@
+
+int bar(const char* s) {}
+
+int foo_fail1(int a, int b) {
+  if (a) {
+      bar("ss");
+      goto bb;
+  } else if (b) {
+      bar("lll");
+      bb:
+      bar("lfff");
+  } else {
+      bar("uuu");
+      bb:
+      bar("lffppppf");
+  }
+}
+
+int foo_fail2(int a, int b) {
+  if (a) {
+      bar("ss");
+      goto cc;
+  } else {
+      bar("uuu");
+      bb:
+      bar("lffppppf");
+  }
+}
