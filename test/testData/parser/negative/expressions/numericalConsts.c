d845ff9b49774f4ab52998c38ca18322def89016
Roman Art <root@romart.dev>
2022-01-30 16:09:54 +0300

    Add tests for numerical constants

---
+++ test/testData/parser/negative/expressions/numericalConsts.c
@@ -0,0 +1,60 @@
+
+void testChar() {
+  char c1 = 'aa';
+  char c2 = 'aas';
+  char c3 = '\aa';
+  char c4 = '\aas';
+  char c5 = '\555';
+  char c6 = '\x80';
+  char c7 = '\0x44';
+  char c8 = '\g';
+}
+
+void testWchar() {
+  int w1 = L'aa';
+  int w2 = L'aas';
+  int w3 = L'\aa';
+  int w4 = L'\aas';
+  int w5 = L'\555555';
+  int w6 = L'\x8000';
+  int w7 = L'\0x44';
+  int w8 = L'\j';
+}
+
+int testInteger() {
+  int s = 12345465456465;
+  unsigned u = 12344545645656u;
+  unsigned u2 = 12545646545463U;
+
+  long l = 2114234411646545641l;
+  long l2 = 21142344654564461L;
+  unsigned long ul1 = 2114235465465441ul;
+  unsigned long ul2 = 2114234456465465641uL;
+  unsigned long ul3 = 21142348465465446461Ul;
+  unsigned long ul4 = 21142345456465465564651UL;
+  unsigned long ul5 = 2114234444654564656461lu;
+  unsigned long ul6 = 2114246546546546465341Lu;
+  unsigned long ul7 = 21142341446546546546lU;
+  unsigned long ul8 = 21142356465465465441LU;
+
+  long long ll = 1232222654546444522ll;
+  long long ll2 = 12322224546546522LL;
+
+  unsigned long long ull1 = 123224544654654652222ull;
+  unsigned long long ull2 = 12322245646545646222uLL;
+  unsigned long long ull3 = 12322254445646546222Ull;
+  unsigned long long ull4 = 445465465465123222222ULL;
+  unsigned long long ull5 = 12322245645665222llu;
+  unsigned long long ull6 = 123222546546546222LLu;
+  unsigned long long ull7 = 123222464566446222llU;
+  unsigned long long ull8 = 12322246546546546546222LLU;
+}
+
+//int testFloatingPoint() {
+//  float f1 = 123564564654564545465456465.0f;
+//  float f2 = 1235444654654654646546465546.0F;
+
+//  double d1 = 431.0;
+//  double d2 = 431.0l;
+//  double d3 = 431.0L;
+//}
---
+++ test/testData/parser/positive/expressions/numericalConsts.c
@@ -0,0 +1,64 @@
+
+void testChar() {
+  char c1 = 'a';
+  char c2 = '\0';
+  char c3 = '\a';
+  char c4 = '\\';
+  char c5 = '\'';
+  char c5 = '\"';
+  char c055 = '\55';
+  char c7f = '\x7f';
+}
+
+void testWchar() {
+  int w1 = L'a';
+  int w2 = L'\0';
+  int w3 = L'\a';
+  int w4 = L'\\';
+  int w5 = L'\'';
+  int w5 = L'\"';
+  int w0555 = L'\555';
+  int w7fff = L'\x7fff';
+}
+
+int testInteger() {
+  int s = 123;
+  unsigned u = 123u;
+  unsigned u2 = 123U;
+
+  int o = 07777;
+  int b = 0b101101010;
+  int BB = 0B1001000101;
+
+  long l = 21142341l;
+  long l2 = 21142341L;
+  unsigned long ul1 = 21142341ul;
+  unsigned long ul2 = 21142341uL;
+  unsigned long ul3 = 21142341Ul;
+  unsigned long ul4 = 21142341UL;
+  unsigned long ul5 = 21142341lu;
+  unsigned long ul6 = 21142341Lu;
+  unsigned long ul7 = 21142341lU;
+  unsigned long ul8 = 21142341LU;
+
+  long long ll = 123222222ll;
+  long long ll2 = 123222222LL;
+
+  unsigned long long ull1 = 123222222ull;
+  unsigned long long ull2 = 123222222uLL;
+  unsigned long long ull3 = 123222222Ull;
+  unsigned long long ull4 = 123222222ULL;
+  unsigned long long ull5 = 123222222llu;
+  unsigned long long ull6 = 123222222LLu;
+  unsigned long long ull7 = 123222222llU;
+  unsigned long long ull8 = 123222222LLU;
+}
+
+int testFloatingPoint() {
+  float f1 = 123.0f;
+  float f2 = 123.0F;
+
+  double d1 = 431.0;
+  double d2 = 431.0l;
+  double d3 = 431.0L;
+}
