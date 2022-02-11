75a26a31583422d2c5b814bfa6e4090301dabf21
Roman Art <root@romart.dev>
2022-02-12 02:01:44 +0300

    Add tests for label pointers
    Update test data

--- test/testData/parser/positive/statements/gotoLabels.c
+++ test/testData/parser/positive/statements/gotoLabels.c
@@ -14,3 +14,19 @@ int foo_ok(int a, int b) {
       goto bb;
   }
 }
+
+int foo_p(int a, int b) {
+  void *lptr = &&bb;
+  if (a) {
+      bar("ss");
+      goto *lptr;
+  } else if(b) {
+      bar("lll");
+      bb:
+      bar("lfff");
+  } else {
+      bar("JHHHH");
+      goto *lptr;
+  }
+}
+
