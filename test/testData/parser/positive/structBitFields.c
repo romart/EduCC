3bae491bcbd85cba5d0ad28a111f5c67eeb6eeb4
Roman Art <root@romart.dev>
2022-02-12 18:41:57 +0300

    Add test about bit field and fix existed tests

---
+++ test/testData/parser/positive/structBitFields.c
@@ -0,0 +1,22 @@
+
+struct S {
+  int if1: 1;
+  int if2: 2;
+  int if3: 3;
+  int if4: 4;
+  int if0: 0;
+  unsigned uf1:10;
+  unsigned uf2:20;
+
+  int x;
+
+  unsigned uf3:30;
+  unsigned uf0:0;
+  unsigned uf4:30;
+  unsigned uf5:30;
+  int if5:1;
+  unsigned uf6:1;
+  long long llf:33;
+
+
+};
