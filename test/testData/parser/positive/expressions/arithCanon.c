f5b180801aa670b719d5f270b950c3270f4187c8
Roman Art <root@romart.dev>
2022-02-19 03:43:25 +0300

    Add test about arith cannonization
    - update test for '%' operator

---
+++ test/testData/parser/positive/expressions/arithCanon.c
@@ -0,0 +1,90 @@
+
+int testInt() {
+  int x = 10;
+  int y = 30;
+  int z = 50;
+
+
+  int a = 5 + x;
+  int b = 0 + x;
+  int c = 0 - x;
+  int d = x + 0;
+  int e = x - 0;
+
+  int f = 2 * x;
+  int h = 1 * x;
+  int i = x * 1;
+
+
+  int j = 5 / x;
+  int k = x / 5;
+  int r = 1 / x;
+  int s = x / 1;
+
+
+  int aa = (x + 5) + 7;
+  int bb = (x - 5) + 7;
+  int cc = (x + 5) - 7;
+  int dd = (x - 5) - 7;
+
+  int ee = (x * 5) * 7;
+  int ff = (x / 5) * 7;
+  int gg = (x * 5) / 7;
+  int hh = (x / 5) / 7;
+
+  int ii = (x % 5) % 7;
+  int jj = (x % 7) % 5;
+
+  int aaa = (x + 5) + y;
+  int bbb = (x + 5) - y;
+  int ccc = (x - 5) + y;
+  int ddd = (x - 5) - y;
+
+  int eee = (x * 5) * y;
+  int fff = (x * 5) / y;
+  int ggg = (x / 5) * y;
+  int hhh = (x / 5) / y;
+}
+
+int testFloat() {
+  float x = 10.0f;
+  float y = 30.0f;
+  float z = 50.0f;
+
+  float a = 5.0f + x;
+  float b = 0.0f + x;
+  float c = 0.f - x;
+  float d = x + 0.f;
+  float e = x - 0.f;
+
+  float f = 2.f * x;
+  float h = 1.f * x;
+  float i = x * 1.f;
+
+
+  float j = 5.f / x;
+  float k = x / 5.f;
+  float r = 1.f / x;
+  float s = x / 1.f;
+
+
+  float aa = (x + 5.f) + 7.f;
+  float bb = (x - 5.f) + 7.f;
+  float cc = (x + 5.f) - 7.f;
+  float dd = (x - 5.f) - 7.f;
+
+  float ee = (x * 5.f) * 7.f;
+  float ff = (x / 5.f) * 7.f;
+  float gg = (x * 5.f) / 7.f;
+  float hh = (x / 5.f) / 7.f;
+
+  float aaa = (x + 5.f) + y;
+  float bbb = (x + 5.f) - y;
+  float ccc = (x - 5.f) + y;
+  float ddd = (x - 5.f) - y;
+
+  float eee = (x * 5.f) * y;
+  float fff = (x * 5.f) / y;
+  float ggg = (x / 5.f) * y;
+  float hhh = (x / 5.f) / y;
+}
