c38bd398f679d02ba065b627161032389be9183d
Roman Art <root@romart.dev>
2022-05-26 02:25:03 +0300

    Fix bunch if issues in PP

--- src/lexer.c
+++ src/lexer.c
@@ -483,13 +483,8 @@ Token *nextToken(ParserContext *ctx) {
 
   if (next->rawCode == IDENTIFIER) {
       Token *d;
-      Token *rnext = replaceMacro(ctx, next, &d);
-      if (rnext != next) {
-        findLastToken(rnext)->next = d;
-        next = rnext;
-      }
+      next = replaceMacro(ctx, next, &d);
       if (prev) prev->next = next;
-
   }
 
   if (next->rawCode == IDENTIFIER) {
