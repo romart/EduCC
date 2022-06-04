d8a3296b8d99fbef376e35e960d6de3461fb814c
Roman Art <root@romart.dev>
2022-06-04 21:13:30 +0300

    Fix alloca test

--- test/testData/codegen/my/alloca.c
+++ test/testData/codegen/my/alloca.c
@@ -1,12 +1,11 @@
-#include <stddef.h>
+#include <stdio.h>
+#include <memory.h>
+#include <alloca.h>
 
-int printf(char *fmt, ...);
 void *fn(int x1, int x2, int x3, int x4, int x5, int x6, int x7, int x8, int x9, int x10, int x11, void *p, int x12, int x13, int x14) {
   printf("%d %d %d %d %d %d %d %d\n", x7, x8, x9, x10, x11, x12, x13, x14);
   return p;
 }
-void *memcpy(void *dest, void *src, long long n);
-int memcmp(char *p, char *q, long long n);
 
 int main() {
   int i = 0;
