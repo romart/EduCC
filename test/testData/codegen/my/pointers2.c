642123df5caea1646b14d992f8f08717e8be2541
Roman Art <root@romart.dev>
2022-06-07 17:06:28 +0300

    Update test data

--- test/testData/codegen/my/pointers2.c
+++ test/testData/codegen/my/pointers2.c
@@ -14,6 +14,12 @@ int test(struct X *p, struct X **pp) {
   return p->a + p->b;
 }
 
+#define ALIGN_SIZE(len, align) ((((align)-1) & (len)) ? (((len)+(align)) & ~((align)-1)) : (len))
+int testPtrAssgn(unsigned *p) {
+  *p = ALIGN_SIZE(*p, 8);
+  return 8;
+}
+
 int main() {
   struct X l = { 18, 33 };
 
@@ -25,6 +31,12 @@ int main() {
 
   if (p->a != 42) return 2;
   if (p->b != 24) return 3;
+
+  unsigned u = 31;
+
+  testPtrAssgn(&u);
+
+  if (u != 32) return 4;
 
   return 0;
 }
