0fba83b11143e412f0e290be6bd2161497b398ea
Roman Art <root@romart.dev>
2022-06-08 03:05:52 +0300

    It's happened!!! We have bootstaped!!! Yahooooo!!
    Now it's time to pick a good name for my compiler

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,30 @@
 
 make clean all
 
-
+rm -rf ./bootstrap
 mkdir -p ./bootstrap
 
-cp build/bin/main bootstrap/main
+cp build/bin/main bootstrap/main_gcc
+rm -rf build
+COMPILER=bootstrap/main_gcc make clean all
 
-COMPILER=bootstrap/main make clean all
+
+
+cp build/bin/main bootstrap/main2
+rm -rf build
+COMPILER=bootstrap/main2 make clean all
+
+
+
+cp build/bin/main bootstrap/main3
+rm -rf build
+COMPILER=bootstrap/main3 make clean all
+
+
+cp build/bin/main bootstrap/main4
+rm -rf build
+COMPILER=bootstrap/main4 make clean all
+
+cp build/bin/main bootstrap/main5
+
+sha1sum ./bootstrap/*
