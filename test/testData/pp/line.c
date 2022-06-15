de40fe5f8279e37004609194679113556c00b222
Roman Art <root@romart.dev>
2022-06-15 19:45:36 +0300

    Add test about #line

---
+++ test/testData/pp/line.c
@@ -0,0 +1,45 @@
+
+
+
+__FILE__ __LINE__
+
+#line 100 "OK"
+
+
+__FILE__ __LINE__
+
+#if 0
+
+#line 200 "ERROR"
+
+__FILE__ __LINE__
+
+#endif
+
+
+#if 0
+
+#line 300 "ERROR"
+
+__FILE__ __LINE__
+
+#else
+
+#line 300 "OK"
+
+__FILE__ __LINE__
+
+#endif
+
+
+#if 1
+
+#line 400 "OK"
+
+__FILE__ __LINE__
+
+#endif
+
+
+__FILE__ __LINE__
+
