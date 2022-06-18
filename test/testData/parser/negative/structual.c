bf7071f84a1a9b15d3983584542e11734826aea8
Roman Art <root@romart.dev>
2022-06-19 02:38:20 +0300

    Add test about duplicated field names

--- test/testData/parser/negative/structual.c
+++ test/testData/parser/negative/structual.c
@@ -19,3 +19,27 @@ struct S3 {
   enum S1 *e1;
   union S1 *u1;
 };
+
+
+struct X {
+  int a;
+  int b;
+  struct {};
+  int c;
+  union {
+    struct {};
+    struct {
+      int a;
+      int b;
+      int d;
+      int e;
+    };
+    struct {
+      struct {};
+    };
+    int c;
+    int d;
+  };
+  struct {};
+  int e;
+};
