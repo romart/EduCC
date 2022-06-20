69908769ee2f1d78fe66689a121b4ace13b81e73
Roman Art <root@romart.dev>
2022-06-20 04:16:05 +0300

    Test about string literal cannonization

--- test/testData/codegen/my/literal.c
+++ test/testData/codegen/my/literal.c
@@ -6,11 +6,15 @@ char test2(int i) {
   return "abc"[i];
 }
 
+char test3(int i) {
+  return "defghijxyz"[i - 2];
+}
 
 int main(int y) {
 
   if (test1() != ':') return 1;
   if (test2(1) != 'b') return 2;
+  if (test3(3) != 'e') return 3;
 
   return 0;
 
