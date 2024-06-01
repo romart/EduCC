906d883f9a99a8a15147b8d39df2cbbc7c72369b
Roman Artemev <roman.artemev@syntacore.com>
2024-06-01 23:18:30 +0300

    Add riscv64 compilation target
     - add CLI option to select arch
     - isolate arch-specific codegen logic into special object

--- include/codegen.h
+++ include/codegen.h
@@ -9,6 +9,8 @@
 #include "tree.h"
 #include "instructions.h"
 #include "_elf.h"
+#include "parser.h"
+#include "sema.h"
 
 typedef struct _GeneratedFunction {
   const char *name;
@@ -66,7 +68,83 @@ typedef struct _GeneratedFile {
   struct _Relocation *relocations;
 } GeneratedFile;
 
-GeneratedFile *generateCodeForFile(struct _ParserContext *ctx, AstFile *astFile);
+struct CaseLabel {
+  int64_t caseConst;
+  struct Label label;
+};
+
+typedef struct _GenerationContext {
+  ParserContext *parserContext;
+
+  GeneratedFile *file;
+
+  Arena *codegenArena;
+
+  Relocation *relocations;
+
+  HashMap *labelMap;
+
+  struct {
+    HashMap *literalMap;
+    HashMap *f4ConstMap;
+    HashMap *f8ConstMap;
+    HashMap *f10ConstMap;
+  } constCache;
+
+  struct Label *continueLabel;
+  struct Label *breakLabel;
+
+  struct Label *defaultLabel;
+
+  unsigned caseCount;
+  struct CaseLabel *caseLabels;
+
+  Symbol *memsetSymbol;
+
+  Section *bss;
+  Section *rodata;
+  Section *data;
+  Section *rodataLocal;
+  Section *dataLocal;
+  Section *text;
+
+} GenerationContext;
+
+GeneratedFile *allocateGenFile(GenerationContext *ctx);
+GeneratedFunction *allocateGenFunction(GenerationContext *ctx);
+GeneratedVariable *allocateGenVarialbe(GenerationContext *ctx, AstValueDeclaration *d);
+Relocation *allocateRelocation(GenerationContext *ctx);
+
+typedef union {
+  uint8_t bytes[sizeof(float)];
+  float f;
+} FloatBytes;
+
+typedef union {
+  uint8_t bytes[sizeof(double)];
+  double d;
+} DoubleBytes;
+
+typedef union {
+  uint8_t bytes[sizeof(long double)];
+  uint64_t qwords[2];
+  long double ld;
+} LongDoubleBytes;
+
+void initConstCache(GenerationContext *ctx);
+void releaseConstCache(GenerationContext *ctx);
+
+void buildElfFile(GenerationContext *ctx, AstFile *astFile, GeneratedFile *genFile, ElfFile *elfFile);
+
+typedef struct _ArchCodegen {
+  GeneratedFunction *(*generateFunction)(GenerationContext *, AstFunctionDefinition *);
+  GeneratedVariable *(*generateVaribale)(GenerationContext *, AstValueDeclaration *);
+} ArchCodegen;
+
+GeneratedFile *generateCodeForFile(struct _ParserContext *ctx, ArchCodegen *archCodegen, AstFile *astFile);
+
+void initArchCodegen_x86_64(ArchCodegen *cg);
+void initArchCodegen_riscv64(ArchCodegen *cg);
 
 void emitByte(GeneratedFunction *f, uint8_t b);
 void emitWord(GeneratedFunction *f, uint16_t w);
---
+++ src/riscv64/codegen_riscv64.c
@@ -0,0 +1,10 @@
+#include "_elf.h"
+#include "codegen.h"
+#include "mem.h"
+#include "parser.h"
+#include "sema.h"
+
+
+void initArchCodegen_riscv64(ArchCodegen *cg) {
+
+}
